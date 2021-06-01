9c58f13d19c2de50164f4754b48087e3649ced1f
bhavesh krishan garg <bhaveshgarg2005@gmail.com>
2021-06-02 00:35:49 +0530

    hellllo

---
+++ day02_2.js
@@ -0,0 +1,10 @@
+//The .forEach() method
+
+
+
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(function(fruitsItem){
+console.log("I want to eat a "+ fruitsItem)
+})
